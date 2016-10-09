19c53ee86922ec7fff30e044f023496f62a924d5
leonj096 <trip.leonard@disney.com>
2016-10-08 22:58:40 -0700

    first comit

---
+++ docker_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo yum update -y
+
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+sudo yum install docker-engine -y
+
+sudo systemctl enable docker.service
+
+sudo systemctl start docker
